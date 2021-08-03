ac31f670a370fc0070b660c4f8d662133d45a2da
Your Name <you@example.com>
2021-08-03 23:34:20 +0700

    fixDetail

--- js/BaiTap4.js
+++ js/BaiTap4.js
@@ -27,6 +27,9 @@ function checkValue(print, value) {
   } else if (length.value == '') {
     print.style.color = 'yellow'
     print.innerHTML = 'Vui lòng nhập giá trị chiều dài hình chữ nhật'
+  } else if (width.value == 0 || length.value == 0) {
+    print.style.color = 'yellow'
+    print.innerHTML = 'Sẽ không có hình chữ nhật để tính'
   } else {
     print.style.color = 'blue'
     print.innerHTML = 'Diện tích hình chữ nhật bằng : ' + value
